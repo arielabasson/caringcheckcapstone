3021105b6c823573e43688d3f10f0d06efb786e0
arielabasson <37116726+arielabasson@users.noreply.github.com>
2020-03-30 09:24:16 -0400

    Add files via upload

---
+++ 3:29/scripts/script.js
@@ -0,0 +1,90 @@
+$(document) .ready(function() {
+  
+/*---------1---------*/
+  
+  $('.arrowdown').click('click touchstart', function(){
+  $(".title").animate({
+    leading: "12px",
+    opacity: 2}, 2000);
+  });
+ 
+  $( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+  
+
+$(document) .ready(function() {
+  
+/*---------1---------*/
+  
+  $('.arrowdown').click('click touchstart', function(){
+  $(".title").animate({
+    leading: "12px",
+    opacity: 2}, 2000);
+  });
+ 
+  $( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+  
+// $(document).ready(function(){
+//     $("#readmore").click(function(){
+//         $("#about").slideToggle("show");
+//         $(this).text(function(i, v){
+//           return v === 'read more' ? 'done' : 'read more'
+//       })
+//     });
+// });
+
+  
+  
+  
+ });
